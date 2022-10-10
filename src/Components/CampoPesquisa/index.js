17ac402d8787f2a2f56c5d5bb803284312d4c803
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-09 22:34:51 -0300

    Criação do componente CampoPesquisa e integração dele na Home

---
+++ src/Components/CampoPesquisa/index.js
@@ -0,0 +1,9 @@
+import './CampoPesquisa.css'
+
+const CampoPesquisa = (props) => {
+    return (
+        <input placeholder={props.placeholder} className='campoPesquisa' />
+    )
+}
+
+export default CampoPesquisa
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,15 @@
+import CampoPesquisa from '../../Components/CampoPesquisa'
 import Header from '../../Components/Header'
 import './Home.css'
 
 const Home = () => {
     return (
-        <Header />
+        <div>
+            <Header />
+            <div className='search__section'>
+                <CampoPesquisa placeholder={'Search for a movie'}/>
+            </div>
+        </div>
     )
 }
 
