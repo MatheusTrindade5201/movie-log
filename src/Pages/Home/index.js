c02c82f38005bab40a173f413e4523bd81fa1843
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-11 19:54:31 -0300

    feat: Add FilterButton na Home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,17 +3,19 @@ import './Home.css'
 import SearchInput from '../../Components/SearchInput'
 import Card from '../../Components/Card'
 import { useEffect, useState } from 'react'
+import FilterButton from '../../Components/FilterButton'
 
 const Home = () => {
 
     const [topRated, setTopReated] = useState(false)
+    const [filter, setFilter] = useState('popular')
 
     const urlBase = process.env.REACT_APP_URL_BASE
     const apiKey = process.env.REACT_APP_API_KEY
 
     const getTopRated = async () => {
         try {
-            const data = await fetch(`${urlBase}top_rated?api_key=${apiKey}`);
+            const data = await fetch(`${urlBase}${filter}?api_key=${apiKey}`);
             const json = await data.json();
             console.log(json);
             setTopReated(json.results)
@@ -23,7 +25,7 @@ const Home = () => {
     }
 
     useEffect(() => {
-        getTopRated();
+        // getTopRated();
     },[])
 
     if(topRated === false){
@@ -32,6 +34,24 @@ const Home = () => {
                 <Header />
                 <div className='search__section'>
                     <SearchInput placeholder={'Search for a movie'}/>
+                </div>
+                <div className='filters'>
+                    <FilterButton
+                     btnValue={'popular'}
+                     changeFilter={value => setFilter(value)}
+                     text={'Popular'} />
+                    <FilterButton
+                     btnValue={'top_rated'}
+                     changeFilter={value => setFilter(value)}
+                     text={'Top Rated'} />
+                    <FilterButton
+                     btnValue={'latest'}
+                     changeFilter={value => setFilter(value)}
+                     text={'Latest'} />
+                    <FilterButton
+                     btnValue={'upcoming'}
+                     changeFilter={value => setFilter(value)}
+                     text={'Upcoming'} />
                 </div>
             </div>
         )
@@ -42,14 +62,14 @@ const Home = () => {
                 <div className='search__section'>
                     <SearchInput placeholder={'Search for a movie'}/>
                 </div>
-                <div className='movies__section'>
+                {/* <div className='movies__section'>
                     {topRated.map(movie => <Card 
                     image={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}
                     name={movie.original_title}
                     release={movie.release_date}
                     rating={movie.vote_average}
                     />)}
-                </div>
+                </div> */}
             </div>
         )
     }
