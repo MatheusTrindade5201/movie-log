9e5ada14d94262296d7414bc761be34596a44a13
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-14 22:27:47 -0300

    env: Troca do metodo de uso do firebase

--- src/auth/config.js
+++ src/auth/config.js
@@ -1,7 +1,7 @@
 import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import { getAuth }from "firebase/auth";
 
-export const authCongig = firebase.initializeApp({
+const authConfig = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -9,5 +9,7 @@ export const authCongig = firebase.initializeApp({
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
+
+export const auth = getAuth(authConfig)
 
 
