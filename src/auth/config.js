b2b8b4f811d3ff6ef6f2e8b5d76a5d44c4806c12
Matheus Trindade <104238483+MatheusTrindade5201@users.noreply.github.com>
2022-10-16 19:07:51 -0300

    fix: chave do firebase

--- src/auth/config.js
+++ src/auth/config.js
@@ -2,7 +2,7 @@ import * as firebase from 'firebase/app';
 import { getAuth }from "firebase/auth";
 
 const authConfig = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apiKey: "AIzaSyB-nwAlBOc0D2j2ksvBgz-fKrIZcsiXYOE",
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
